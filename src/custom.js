d90f53a617aa2c0296e6d4314fdaba244def97a5
yatish <yatishpoojary@ymail.com>
2014-07-30 21:23:38 +0530

    updated demo page and added examples

--- src/custom.js
+++ src/custom.js
@@ -1,21 +1,23 @@
 $(document).ready(function() {
   var text;
 
-  $('.readonly').shortened();
+  $('.jt-default').shortened();
+  $('.jt-bip').shortened();
+  $('.jt-destroy').shortened();
 
   $('button').click(function() {
-    $('#destroy .readonly').shortened('destroy');
+    $('.jt-destroy').shortened('destroy');
   });
 
-  $('#bip .readonly').best_in_place();
+  $('.jt-bip').best_in_place();
 
   $(document).on('focusout', 'input', function() {
     text = $(this).val();
   });
 
   $(document).on("best_in_place:error", function(event, request, error) {
-      $('#bip .readonly').shortened('destroy');
-      $('#bip .readonly').text(text);
-      $('#bip .readonly').shortened();
+      $('.jt-bip').shortened('destroy');
+      $('.jt-bip').text(text);
+      $('.jt-bip').shortened();
   });
 });

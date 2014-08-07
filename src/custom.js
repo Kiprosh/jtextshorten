bfc1932e2597477ac88d8489ff6e1452bb438b88
yatish <yatishpoojary@ymail.com>
2014-08-07 18:55:46 +0530

    Refactored changes

--- src/custom.js
+++ src/custom.js
@@ -1,36 +1,33 @@
-$(document).ready(function() {
-  var text;
+$( document ).ready(function() {
+  var text, data;
+  $( '.jt-default' ).shortened();
+  $( '.jt-bip' ).shortened();
+  $( '.jt-destroy' ).shortened();
+  $( '.jt-limitchar' ).shortened( { limitChars: 100 } );
+  $( 'button' ).click(function() {
+    $( '.jt-destroy' ).shortened( 'destroy' );
+  });
+  $( '.jt-bip' ).best_in_place();
 
-  $('.jt-default').shortened();
-  $('.jt-bip').shortened();
-  $('.jt-destroy').shortened();
-  $('.jt-limitchar').shortened({limitChars: 100});
-
-  $('button').click(function() {
-    $('.jt-destroy').shortened('destroy');
+  $( document ).on( 'focusout', 'input', function() {
+    text = $( this ).val();
   });
 
-  $('.jt-bip').best_in_place();
-
-  $(document).on('focusout', 'input', function() {
-    text = $(this).val();
+  $( document ).on( 'focusin', 'input', function() {
+    data = $( this ).closest( '.jt-bip' ).attr( 'data-content' );
+    $( this ).val( data );
   });
 
-  $(document).on('focusin', 'input', function() {
-    var data = $(this).closest('.jt-bip').attr('data-content');
-    $(this).val(data);
-  });
-
-  $(document).on("best_in_place:error", function(event, request, error) {
-    if (text != ""){
-      $('.jt-bip').text(text);
-      $('.jt-bip').attr('data-content', text);
+  $( document ).on( "best_in_place:error", function( event, request, error ) {
+    if ( text != "" ){
+      $( '.jt-bip' ).text( text );
+      $( '.jt-bip' ).attr( 'data-content', text );
     }
-    else{
-      $('.jt-bip').text("default");
-      $('.jt-bip').attr('data-content', "default");
+    else {
+      $( '.jt-bip' ).text( "default" );
+      $( '.jt-bip' ).attr( 'data-content', "default" );
     }
-    $('.jt-bip').shortened('destroy');
-    $('.jt-bip').shortened();
+    $( '.jt-bip' ).shortened( 'destroy' );
+    $( '.jt-bip' ).shortened();
   });
 });
--- src/jquery.jTextShorten.js
+++ src/jquery.jTextShorten.js
@@ -1,102 +1,110 @@
-(function($){
+(function( $ ){
   DEFAULT_CLASS = 'ui-shortened';
   EMPTY         = 0;
-
-  var _defaults, methods, see_more_tag;
-
-  var _defaults = {
-    lessText:       'See less', // Display text when expanded
-    limitChars:     300,        // Limit characters to truncate
-    moreText:       'See more'  // Display text when string is truncated
-  };
-
+  var methods, seeMoreTag,
+      _defaults = {
+        lessText:   'See less',  // Display text when expanded
+        limitChars:  300,        // Limit characters to truncate
+        moreText:   'See more'   // Display text when string is truncated
+      };
   methods = {
-    init: function(options)
+    init: function( options ){
+      return this.each(function()
       {
-        return this.each(function()
-        {
-          var $this = $(this);
-          var uniq_stamp = generateUniqStamp();
-          var settings = $.extend({}, _defaults);
-          var excluded_tags = ["INPUT", "IMG", "SELECT", "IFRAME"];
-          settings.unique_identity = uniq_stamp;
-          if($.inArray(this.tagName, excluded_tags) == -1) {
-            if (options) {
-              settings   = $.extend(settings, options);
-              settings.limitChars = parseInt(settings.limitChars);
-            }
-            if (!$.data(this, 'shortened'))
-              $this.data('shortened', settings);
-            determineTextLength($this, uniq_stamp, settings);
+        var tag,
+            $this = $( this ),
+            uniq_stamp = generateUniqStamp(),
+            settings = $.extend( {}, _defaults ),
+            excluded_tags = [ "INPUT", "IMG", "SELECT", "IFRAME" ];
+        settings.unique_identity = uniq_stamp;
+        if( $.inArray( this.tagName, excluded_tags ) == -1 ) {
+          if ( options ) {
+            settings   = $.extend( settings, options );
+            settings.limitChars = parseInt( settings.limitChars );
           }
-          else{
-            var tag = $(this).prop("tagName");
-            throw new Error("jQuery.shortened cannot be applied on " + tag + " tag.");
-          }
-        });
-      },
-
-      destroy: function() {
-        $this = $(this);
-        $this.siblings('.ui-see_more, .ui-see_less').remove();
-        $this.removeClass(DEFAULT_CLASS);
-        $this.removeData('shortened');
-        return $this;
-      }
+          if ( !$.data( this, 'shortened' ) )
+            $this.data( 'shortened', settings );
+          determineTextLength( $this, uniq_stamp, settings );
+        }
+        else{
+          tag = $( this ).prop( "tagName" );
+          throw new Error( "jQuery.shortened cannot be applied on " + tag +
+            " tag." );
+        }
+      });
+    },
+    destroy: function(){
+      $this = $( this );
+      $this.siblings( '.ui-see_more, .ui-see_less' ).remove();
+      $this.removeClass( DEFAULT_CLASS );
+      $this.removeData( 'shortened' );
+      return $this;
+    }
   };
 
-  function collapse(element) {
-    var options =  element.data('shortened');
-    $(".ui-see_less[data-uniq=" + options.unique_identity + "]").removeClass('ui-see_less').
-      addClass('ui-see_more').text(options.moreText);
-    $(element).addClass(DEFAULT_CLASS);
-    var text = element.text().substr(0, options['limitChars']);
-    element.html(text);
+  function collapse( element ){
+    var text,
+        options =  element.data( 'shortened' );
+    $( ".ui-see_less[data-uniq=" + options.unique_identity + "]" ).
+      removeClass( 'ui-see_less' ).addClass( 'ui-see_more' ).
+      text( options.moreText );
+    $( element ).addClass( DEFAULT_CLASS );
+    text = element.text().substr( 0, options[ 'limitChars' ] );
+    element.html( text );
   };
 
-  function expand(element) {
-    var options =  element.data('shortened');
-    $(".ui-see_more[data-uniq=" + options.unique_identity + "]").removeClass('ui-see_more').
-      addClass('ui-see_less').text(options.lessText);
-    $(element).removeClass(DEFAULT_CLASS);
-    $(element).html($(element).attr('data-content'));
+  function expand( element ){
+    var options =  element.data( 'shortened' );
+    $( ".ui-see_more[data-uniq=" + options.unique_identity + "]" ).
+      removeClass( 'ui-see_more' ).addClass( 'ui-see_less' ).
+      text( options.lessText );
+    $( element ).removeClass( DEFAULT_CLASS );
+    $( element ).html( $( element ).attr( 'data-content' ) );
   };
 
   function generateUniqStamp() {
-    return Math.round(Math.random()*1E16);
+    return Math.round( Math.random()*1E16 );
   }
 
-  function determineTextLength(element, stamp, settings) {
-    if (element.text().length > settings.limitChars) {
-      element.addClass(DEFAULT_CLASS);
-      if (element.next('.ui-see_more, .ui-see_less').length > EMPTY) {
-        element.next('.ui-see_more, .ui-see_less').remove();
+  function determineTextLength( element, stamp, settings ){
+    if ( element.text().length > settings.limitChars ) {
+      element.addClass( DEFAULT_CLASS );
+      if ( element.next( '.ui-see_more, .ui-see_less' ).length > EMPTY ){
+        element.next( '.ui-see_more, .ui-see_less' ).remove();
       }
-      element.attr({'data-uniq': stamp, 'data-content': element.text()});
-      var text = element.text().substr(0, settings.limitChars);
-      element.html(text);
-      see_more_tag = "<span class='ui-see_more ui-blue' data-uniq=" + stamp + ">See more</span>";
-      $(see_more_tag).insertAfter(element);
+      element.attr( {'data-uniq': stamp, 'data-content': element.text()} );
+      var text = element.text().substr( 0, settings.limitChars );
+      element.html( text );
+      seeMoreTag = "<span class='ui-see_more ui-blue' data-uniq=" + stamp +
+        ">See more</span>";
+      $( seeMoreTag ).insertAfter( element );
     }
   }
 
   // Click on see more
-  $(document).on( 'click', '.ui-see_more', function(e) {
-    var shortenedDiv = $(this).prev();
-    expand(shortenedDiv);
+  $( document ).on( 'click', '.ui-see_more', function( e ){
+    var shortenedDiv = $( this ).prev();
+    expand( shortenedDiv );
   });
 
   // Click on see less
-  $(document).on( 'click', '.ui-see_less', function(e) {
-    var shortenedDiv = $(this).prev();
-    collapse(shortenedDiv);
+  $( document ).on( 'click', '.ui-see_less', function( e ){
+    var shortenedDiv = $( this ).prev();
+    collapse( shortenedDiv );
   });
 
   // Plugin entry
-  $.fn.shortened = function(method)
+  $.fn.shortened = function( method )
   {
-    if(methods[method]) { return methods[method].apply(this, Array.prototype.slice.call(arguments, 1)); }
-    else if(typeof method === 'object' || !method) { return methods.init.apply(this, arguments); }
-    else { $.error('Method ' + method + ' does not exist on jQuery.shortened'); }
+    if( methods[ method ] ){
+      return methods[ method ].
+        apply( this, Array.prototype.slice.call( arguments, 1 ) );
+    }
+    else if( typeof method === 'object' || !method ){
+      return methods.init.apply( this, arguments );
+    }
+    else{
+      $.error( 'Method ' + method + ' does not exist on jQuery.shortened' );
+    }
   }
 }(jQuery));
